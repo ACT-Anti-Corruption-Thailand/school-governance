627f1104ca482064e6485e7448693a77d531cf63
rootEnginear <rootEnginear@users.noreply.github.com>
2022-11-22 14:47:20 +0700

    chore: set options to spa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	let count = 0;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<hr />
+<div>{count}</div>
+<button
+	type="button"
+	on:click={() => {
+		count++;
+	}}
+>
+	Increment
+</button>
