2a9c216702777f7327f6f0b3a6d115e78733642e
rootEnginear <rootEnginear@users.noreply.github.com>
2022-12-08 00:04:45 +0700

    feat: add mobile & basic desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,26 @@
 <header class="f header">
 	<div />
 	<p>
-		อยากเห็นโรงเรียนของพวกเรา<br />
+		อยากเห็นโรงเรียนของพวกเรา<br class="hide-desktop" />
 		เปลี่ยนไปในทางที่ดีขึ้นไหม?
 	</p>
 	<img class="header-logo" src="/logo.svg" alt="" width="224" height="83" />
 	<p>
-		ชวนทุกคนมาแลกเปลี่ยน<br />
+		ชวนทุกคนมาแลกเปลี่ยน<br class="hide-desktop" />
 		เพื่อพัฒนาโรงเรียนไปด้วยกัน
 	</p>
 	<div class="f header-detail">
 		<div class="f header-detail-col">
 			<img src="/mascots/data.svg" alt="" width="40" height="40" />
-			<span>ส่องฐาน<br />ข้อมูล</span>
+			<span>ส่องฐาน<br class="hide-desktop" />ข้อมูล</span>
 		</div>
 		<div class="f header-detail-col">
 			<img src="/mascots/star.svg" alt="" width="40" height="40" />
-			<span>ให้คะแนน<br />โรงเรียน</span>
+			<span>ให้คะแนน<br class="hide-desktop" />โรงเรียน</span>
 		</div>
 		<div class="f header-detail-col">
 			<img src="/mascots/speak.svg" alt="" width="40" height="40" />
-			<span>แลกเปลี่ยน<br />ความคิดเห็น</span>
+			<span>แลกเปลี่ยน<br class="hide-desktop" />ความคิดเห็น</span>
 		</div>
 	</div>
 	<a class="f search-box" href="/search">
@@ -36,6 +36,7 @@
 		<span>ลองค้นหาโรงเรียนคุณ</span>
 	</a>
 </header>
+
 <section class="school-list-container">
 	<h2 class="school-list-header">หรือดูโรงเรียนอื่นที่สนใจ</h2>
 	<div class="f choice-con">
@@ -260,7 +261,7 @@
 	<h2 class="f jcc">
 		<img src="/index/q1.svg" alt="" width="38" height="39" />
 		<span>
-			School Governance<br />คืออะไร?<br />
+			School Governance <br class="hide-desktop" />คืออะไร?<br />
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 15 8" width="15" height="8"
 				><path
 					stroke="#16338F"
@@ -295,58 +296,59 @@
 	<p class="info-title2">
 		แต่ไม่ใช่ทุกคำเรียกร้องที่จะไปได้ไกล เกินกว่าพาดหัวข่าว หรือ ข้อความในโลกอินเทอร์เน็ต
 	</p>
-	<p class="info-title3">การมีส่วนร่วม<br />จึงเป็นกุญแจสำคัญ…</p>
+	<p class="info-title3">การมีส่วนร่วม<br class="hide-desktop" />จึงเป็นกุญแจสำคัญ…</p>
 	<p>
 		การจะผลักดันให้โรงเรียนพัฒนาต่อไปได้ ต้องอาศัย <strong>การมีส่วนร่วม</strong>
 		ซึ่งไม่ได้มาจากแค่นักเรียนอย่างพวกเราเพียงฝ่ายเดียว แต่ผู้อำนวยการ คุณครู บุคลากร ฯลฯ ก็เป็นกำลังสำคัญในการร่วมรับรู้ข้อมูลของโรงเรียน
 		ก่อนนำไปสู่การแลกเปลี่ยน แสดงความคิดเห็นและแก้ปัญหาเพื่อพัฒนาโรงเรียนไปพร้อมกับพวกเราด้วย
 	</p>
 	<img src="/index/contribute.svg" alt="" />
-	<div>
-		<p>นี่คือที่มาของ</p>
-		<img src="" alt="" />
-		<p>
-			เครื่องมือที่จะแสดง <a href="https://data.bopp-obec.info/emis/index.php">ฐานข้อมูลโรงเรียน</a>
-			ซึ่งรวบรวมโดยสำนักงานคณะกรรมการการศึกษาขั้นพื้นฐาน (สพฐ.) เพื่อช่วยให้ทุกคนสามารถตรวจสอบข้อมูลสำคัญต่าง
-			ๆ ที่เกี่ยวข้องกับโรงเรียนได้
-		</p>
-		<h3 class="f">
-			<img src="" alt="" />
-			<span>ส่องข้อมูลโรงเรียนของเรา</span>
-			<img src="" alt="" />
-		</h3>
-		<div class="data-cards">
-			<div class="card">
-				<h4>ข้อมูลพื้นฐานโรงเรียน</h4>
-				<p>เช็ครหัส ที่อยู่ เบอร์โทรโรงเรียน ฯลฯ</p>
-			</div>
-			<div class="card">
-				<h4>ข้อมูลนักเรียน</h4>
-				<p>นับจำนวนเพื่อนพี่น้องร่วมสถาบัน</p>
-			</div>
-			<div class="card">
-				<h4>ข้อมูลบุคลากร</h4>
-				<p>เช็คจำนวนครูมีสัดส่วนเท่าไหร่เมื่อเทียบกับนักเรียน</p>
-			</div>
-			<div class="card">
-				<h4>ข้อมูลอุปกรณ์</h4>
-				<p>ตรวจดูวัสดุ อุปกรณ์การเรียนต่าง ๆ ใช้งานได้จริงครบถ้วนหรือไม่</p>
-			</div>
-			<div class="card">
-				<h4>ข้อมูลสิ่งปลูกสร้าง</h4>
-				<p>เช็คสภาพสิ่งปลูกสร้าง จำนวนห้องเรียน ตรงกับความเป็นจริงแค่ไหน</p>
-			</div>
-		</div>
-		<p>
-			พร้อมเทียบข้อมูลระหว่างปีการศึกษา เมื่อมีการอัปเดตข้อมูลใหม่ และ Download ไปใช้งานต่อในรูปแบบ
-			CSV ได้
-		</p>
-	</div>
 </section>
 
-<section>
+<section class="origin">
+	<p>นี่คือที่มาของ</p>
+	<img class="header-logo" src="/logo.svg" alt="" width="224" height="83" />
+	<p>
+		เครื่องมือที่จะแสดง <a href="https://data.bopp-obec.info/emis/index.php">ฐานข้อมูลโรงเรียน</a>
+		ซึ่งรวบรวมโดยสำนักงานคณะกรรมการการศึกษาขั้นพื้นฐาน (สพฐ.) เพื่อช่วยให้ทุกคนสามารถตรวจสอบข้อมูลสำคัญต่าง
+		ๆ ที่เกี่ยวข้องกับโรงเรียนได้
+	</p>
+	<h3 class="f jcc">
+		<img src="/mascots/data.svg" alt="" width="40" height="40" />
+		<span>ส่องข้อมูลโรงเรียนของเรา</span>
+		<img src="/mascots/data.svg" alt="" width="40" height="40" style="transform:scaleX(-100%)" />
+	</h3>
+	<div class="f data-cards">
+		<div class="card">
+			<h4>ข้อมูลพื้นฐานโรงเรียน</h4>
+			<p>เช็ครหัส ที่อยู่ เบอร์โทรโรงเรียน ฯลฯ</p>
+		</div>
+		<div class="card">
+			<h4>ข้อมูลนักเรียน</h4>
+			<p>นับจำนวนเพื่อนพี่น้องร่วมสถาบัน</p>
+		</div>
+		<div class="card">
+			<h4>ข้อมูลบุคลากร</h4>
+			<p>เช็คจำนวนครูมีสัดส่วนเท่าไหร่เมื่อเทียบกับนักเรียน</p>
+		</div>
+		<div class="card">
+			<h4>ข้อมูลอุปกรณ์</h4>
+			<p>ตรวจดูวัสดุ อุปกรณ์การเรียนต่าง ๆ ใช้งานได้จริงครบถ้วนหรือไม่</p>
+		</div>
+		<div class="card">
+			<h4>ข้อมูลสิ่งปลูกสร้าง</h4>
+			<p>เช็คสภาพสิ่งปลูกสร้าง จำนวนห้องเรียน ตรงกับความเป็นจริงแค่ไหน</p>
+		</div>
+	</div>
+	<p>
+		พร้อมเทียบข้อมูลระหว่างปีการศึกษา เมื่อมีการอัปเดตข้อมูลใหม่ และ Download ไปใช้งานต่อในรูปแบบ
+		CSV ได้
+	</p>
+</section>
+
+<section class="f open-join-learn">
 	<h2>
-		การเปิดฐานข้อมูลโรงเรียน<br />
+		การเปิดฐานข้อมูลโรงเรียน<br class="hide-desktop" />
 		เป็นส่วนหนึ่งของ<br />
 		Open Join Learn
 	</h2>
@@ -357,46 +359,68 @@
 			ยังต้องมีพื้นที่ สำหรับตรวจสอบ รายงาน ไปจนถึงสามารถ แลกเปลี่ยนความคิดเห็นร่วมกันได้
 		</strong>
 	</p>
-	<img src="" alt="" />
+	<img src="/index/openjoinlearn.svg" alt="" />
 	<p>
 		การสร้างพื้นที่ปลอดภัยให้ทุกคนมีส่วนร่วมจึงสำคัญมาก ไม่เพียงเพื่อให้โรงเรียนก้าวไปข้างหน้าได้
 		<strong>แต่เพื่อส่งเสริมให้เกิดบรรยากาศของการบริหารจัดการที่โปร่งใส</strong> และเราในฐานะนักเรียนก็จะได้
 		ฝึกฝนบทบาทการตั้งคำถามตรวจสอบและ แลกเปลี่ยนความคิดเห็นไปด้วย…ก่อนก้าวไปเป็น พลเมืองในโลกแห่งความเป็นจริง
 	</p>
-	<div>
-		<p>
-			School Governace จึงไม่ได้มีแค่ข้อมูลโรงเรียนให้ได้ส่องกันเท่านั้น แต่เพื่อน ๆ
-			ยังสามารถเข้ามาช่วยกัน เปรียบเทียบสิ่งอำนวยความสะดวกต่าง ๆ ในโรงเรียนว่าได้มาตรฐานหรือไม่
-			ไปจนถึงส่ง Feedback กลับไปยังโรงเรียนและผู้บริหารที่มีส่วนเกี่ยวข้องกับโรงเรียนของเรา
-		</p>
-		<div class="feature-card-container">
-			<div class="feature-card">
-				<h3>
-					<img src="" alt="" />
-					<span>ให้คะแนนโรงเรียน</span>
-					<img src="" alt="" />
-				</h3>
-				<p>เทียบบริการในโรงเรียนกับเกณฑ์มาตรฐาน พร้อมประเมิน ให้คะแนนโรงเรียนในแต่ละด้าน</p>
-			</div>
-			<div class="feature-card">
-				<h3>
-					<img src="" alt="" />
-					<span>โรงเรียนในมุมของเรา</span>
-					<img src="" alt="" />
-				</h3>
-				<p>
-					แสดงความคิดเห็นโดยไม่ระบุตัวตน ส่งประเด็น ปัญหาต่าง ๆ ไปยังบุคคลที่เกี่ยวข้อง
-					ดูนโยบายข้อมูลส่วนบุคคล
-				</p>
-			</div>
-			<div class="feature-card">
-				<h3>
-					<img src="" alt="" />
-					<span>ติดตามประกาศ</span>
-					<img src="" alt="" />
-				</h3>
-				<p>ช่องทางสำหรับสื่อสารหรือตอบกลับข้อคิดเห็นของเพื่อน ๆ โดยตรงจากโรงเรียน</p>
-			</div>
+</section>
+
+<section class="f feature">
+	<p>
+		School Governace จึงไม่ได้มีแค่ข้อมูลโรงเรียนให้ได้ส่องกันเท่านั้น แต่เพื่อน ๆ
+		ยังสามารถเข้ามาช่วยกัน
+		<strong
+			>เปรียบเทียบสิ่งอำนวยความสะดวกต่าง ๆ ในโรงเรียนว่าได้มาตรฐานหรือไม่ ไปจนถึงส่ง Feedback</strong
+		>
+		กลับไปยังโรงเรียนและผู้บริหารที่มีส่วนเกี่ยวข้องกับโรงเรียนของเรา
+	</p>
+	<div class="f feature-card-container">
+		<div class="feature-card">
+			<h3 class="f">
+				<img src="/mascots/star.svg" alt="" width="40" height="40" />
+				<span>ให้คะแนนโรงเรียน</span>
+				<img
+					src="/mascots/star.svg"
+					alt=""
+					width="40"
+					height="40"
+					style="transform:scaleX(-100%)"
+				/>
+			</h3>
+			<p>เทียบบริการในโรงเรียนกับเกณฑ์มาตรฐาน พร้อมประเมิน ให้คะแนนโรงเรียนในแต่ละด้าน</p>
+		</div>
+		<div class="feature-card">
+			<h3 class="f">
+				<img src="/mascots/speak.svg" alt="" width="40" height="40" />
+				<span>โรงเรียนในมุมของเรา</span>
+				<img
+					src="/mascots/speak.svg"
+					alt=""
+					width="40"
+					height="40"
+					style="transform:scaleX(-100%)"
+				/>
+			</h3>
+			<p>
+				แสดงความคิดเห็นโดยไม่ระบุตัวตน ส่งประเด็น ปัญหาต่าง ๆ ไปยังบุคคลที่เกี่ยวข้อง
+				ดูนโยบายข้อมูลส่วนบุคคล
+			</p>
+		</div>
+		<div class="feature-card">
+			<h3 class="f">
+				<img src="/mascots/exclaim.svg" alt="" width="40" height="40" />
+				<span>ติดตามประกาศ</span>
+				<img
+					src="/mascots/exclaim.svg"
+					alt=""
+					width="40"
+					height="40"
+					style="transform:scaleX(-100%)"
+				/>
+			</h3>
+			<p>ช่องทางสำหรับสื่อสารหรือตอบกลับข้อคิดเห็นของเพื่อน ๆ โดยตรงจากโรงเรียน</p>
 		</div>
 	</div>
 	<a class="f search-box" href="/search">
@@ -413,45 +437,88 @@
 	</a>
 </section>
 
-<section>
+<section class="f ending">
 	<p>
 		แชร์ให้เพื่อน ๆ มาร่วมตรวจสอบด้วยกัน<br />
 		<strong>#schoolgovernanceTH</strong>
 	</p>
-	<div class="f">
-		<a href=""><img src="" alt="" /></a>
-		<a href=""><img src="" alt="" /></a>
-		<a href=""><img src="" alt="" /></a>
+	<div class="f social-links">
+		<a href="">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 40 41"
+				width="40"
+				height="40"
+				aria-label="Facebook"
+				><path
+					fill="#3C55AB"
+					d="M20.079.104C8.924.104 0 9.064 0 20.264c0 10.08 7.33 18.4 16.892 19.84v-14.08h-5.1v-5.76h5.1v-4.48c0-5.12 3.027-7.84 7.649-7.84 2.23 0 4.461.32 4.461.32v4.96h-2.55c-2.549 0-3.346 1.6-3.346 3.2v3.84h5.578l-.956 5.76h-4.622v14.08c9.562-1.44 16.892-9.92 16.892-19.84.16-11.2-8.765-20.16-19.92-20.16z"
+				/></svg
+			>
+		</a>
+		<a href="">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 40 41"
+				width="40"
+				height="40"
+				aria-label="Twitter"
+				><path
+					fill="#6BC9FF"
+					d="M34.127 5.978C30.477 2.327 25.555.104 20 .104c-5.556 0-10.476 2.223-14.127 5.874C2.223 9.628 0 14.548 0 20.105c0 10.317 7.936 18.888 18.095 19.84.635 0 1.27.16 1.905.16 1.429 0 2.698-.16 3.968-.477 3.968-.793 7.46-2.698 10.159-5.397C37.777 30.581 40 25.66 40 20.104c0-5.555-2.222-10.476-5.873-14.126zm0 6.031c-.318.476-.794.953-1.111 1.429-.476.476-.953.793-1.429 1.27l-.158.158v2.223c0 .476-.16.793-.16 1.27-.158.793-.317 1.587-.634 2.222-.159.476-.318.952-.635 1.428s-.476 1.111-.794 1.587c-.317.636-.793 1.112-1.11 1.747-.318.476-.636.793-1.112 1.27-.476.476-.952.952-1.429 1.27-.317.158-.476.476-.793.634-.635.476-1.27.794-1.905 1.111-.476.159-.952.477-1.428.635-.635.318-1.429.476-2.223.635-.476.159-.952.159-1.428.159H15.397c-.318 0-.635 0-.953-.159-.476 0-1.11-.159-1.587-.317-.635-.159-1.111-.318-1.746-.476-.635-.16-1.111-.477-1.587-.635-.476-.318-.953-.477-1.429-.794h2.064c.317 0 .635 0 .793-.159.953-.159 1.746-.317 2.54-.793.635-.318 1.27-.635 1.905-1.112.159-.158.317-.158.476-.317-.635 0-1.111-.159-1.587-.318-.476-.158-.953-.476-1.429-.793-.476-.318-.793-.794-1.111-1.111-.317-.477-.794-1.27-.794-1.429.477 0 .794.159 1.27.159s1.111-.159 1.27-.159c-.476-.159-.794-.317-1.27-.476l-1.428-.952c-.476-.477-.794-.953-.953-1.429-.158-.476-.317-.794-.476-1.27-.159-.317-.159-.793-.159-1.11v-.16c.477.318 1.111.477 1.746.635h.635a2.19 2.19 0 00-.635-.476c-.476-.476-.952-.952-1.27-1.587-.317-.635-.476-1.429-.634-2.223v-.634c0-.477.158-.794.317-1.27l.476-.953c.318.318.477.635.794.794l.952.952c.476.477.953.794 1.429 1.112a10.12 10.12 0 002.222 1.27c.476.158.953.476 1.429.634.793.318 1.587.476 2.38.635.477 0 .794.159 1.27.159h.635c.16 0 .16 0 .16-.159 0-.317-.16-.635-.16-.952v-.794c.16-.635.318-1.27.635-1.905.318-.476.635-.952 1.111-1.428a3.457 3.457 0 011.27-.794c.635-.317 1.429-.476 2.223-.476h1.111c.793.159 1.428.476 2.063.794.318.158.635.476.794.635h.158c.16 0 .318-.16.635-.16.477-.158.794-.317 1.27-.475.476-.16.953-.476 1.429-.794-.159.317-.318.794-.476 1.111-.16.318-.477.635-.635.953-.16.158-.477.476-.635.634-.159.16-.318.16-.476.318h.317c.635-.159 1.111-.159 1.746-.476 0-.159.318-.159.635-.318z"
+				/></svg
+			>
+		</a>
+		<a href="">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 40 41"
+				width="40"
+				height="40"
+				aria-label="Line"
+				><path
+					fill="#00B900"
+					fill-rule="evenodd"
+					d="M40 20.105c0 11.045-8.954 20-20 20s-20-8.955-20-20c0-11.046 8.954-20 20-20s20 8.954 20 20zm-20.012-12c7.362 0 13.35 4.862 13.35 10.837 0 2.387-.925 4.538-2.863 6.675-2.8 3.225-9.062 7.15-10.487 7.75-1.408.593-1.23-.363-1.165-.712l.002-.013c.025-.2.188-1.138.188-1.138.05-.35.1-.875-.038-1.212-.15-.375-.737-.563-1.175-.663-6.412-.85-11.162-5.337-11.162-10.687 0-5.975 5.987-10.838 13.35-10.838z"
+					clip-rule="evenodd"
+				/><path
+					fill="#00B900"
+					d="M28.9 22.394a.25.25 0 00.25-.25v-.95a.25.25 0 00-.25-.25h-2.55v-.987h2.55a.25.25 0 00.25-.25v-.95a.25.25 0 00-.25-.25h-2.55v-.988h2.55a.25.25 0 00.25-.25v-.95a.25.25 0 00-.25-.25h-3.75a.25.25 0 00-.25.25v5.813c0 .137.112.25.25.25h3.75v.012zM15.024 22.394a.25.25 0 00.25-.25v-.95a.25.25 0 00-.25-.25h-2.55V16.32a.25.25 0 00-.25-.25h-.95a.25.25 0 00-.25.25v5.825c0 .138.113.25.25.25h3.75zM17.274 16.056h-.95a.25.25 0 00-.25.25v5.825c0 .137.113.25.25.25h.95a.25.25 0 00.25-.25v-5.825c.013-.138-.1-.25-.25-.25zM23.724 16.056h-.95a.25.25 0 00-.25.25v3.462l-2.662-3.6c0-.012-.013-.012-.025-.025l-.013-.012-.012-.013h-.013l-.012-.012h-.013s-.012 0-.012-.013h-.013s-.012 0-.012-.012h-1.038a.25.25 0 00-.25.25v5.825c0 .137.113.25.25.25h.95a.25.25 0 00.25-.25v-3.463l2.675 3.6a.15.15 0 00.063.063l.012.012h.063c.025 0 .037.013.062.013h.938a.25.25 0 00.25-.25v-5.825a.233.233 0 00-.238-.25z"
+				/></svg
+			>
+		</a>
 	</div>
-	<span>ติดตามข้อมูลข่าวสารเพิ่มเติมได้ที่</span>
-	<a href="https://www.facebook.com/wethestudentsTH">Facebook: We The students Thailand</a>
-	<a href="https://twitter.com/wethestudentst1">Twitter: We The Student TH</a>
+	<div class="f more-links">
+		<span class="bold">ติดตามข้อมูลข่าวสารเพิ่มเติมได้ที่</span>
+		<a href="https://www.facebook.com/wethestudentsTH">Facebook: We The students Thailand</a>
+		<a href="https://twitter.com/wethestudentst1">Twitter: We The Student TH</a>
+	</div>
 </section>
 
-<footer>
-	<div class="f">
+<footer class="f footer">
+	<div class="f credit">
 		<span>Powered by</span>
-		<img src="" alt="" />
+		<img src="/index/act.svg" alt="" width="32" height="24" />
 	</div>
-	<div class="f">
+	<div class="f credit">
 		<span>Co-Developed by</span>
-		<img src="" alt="" />
-		<img src="" alt="" />
+		<img src="/index/pu.svg" alt="" width="50" height="16" />
+		<img src="/index/hand.png" alt="" width="42" height="16" />
 	</div>
-	<div class="f">
+	<div class="f credit">
 		<span>Sponsors by </span>
-		<img src="" alt="" />
-		<img src="" alt="" />
-		<img src="" alt="" />
+		<img src="/index/hand.png" alt="" width="42" height="16" />
+		<img src="/index/hand.png" alt="" width="42" height="16" />
+		<img src="/index/hand.png" alt="" width="42" height="16" />
 	</div>
 	<p>
 		School Governance ต่อยอดจากแพลตฟอร์ม We The Students
 		หนึ่งในผลการวิจัยภายใต้ชุดโครงการวิจัยเพื่อสังคมไทยไร้คอร์รัปชัน ระยะที่ 2 โดย ผศ. ดร.ธานี
-		ชัยวัฒน์ และคณะ
-	</p>
-	<p>
-		คณะเศรษฐศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย สนับสนุนโดย สำนักงานการวิจัยแห่งชาติ (วช.)
-		และสำนักงานคณะกรรมการส่งเสริมวิทยาศาสตร์ วิจัยและนวัตกรรม (สกสว.)
+		ชัยวัฒน์ และคณะ<br class="hide-desktop" /> คณะเศรษฐศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย สนับสนุนโดย สำนักงานการวิจัยแห่งชาติ
+		(วช.) และสำนักงานคณะกรรมการส่งเสริมวิทยาศาสตร์ วิจัยและนวัตกรรม (สกสว.)
 	</p>
 </footer>
 
@@ -596,12 +663,14 @@
 
 			.school-list-title {
 				font-family: 'Mitr';
-				font-weight: 500;
+				font-weight: 700;
 				font-size: 0.8125rem;
 				line-height: 125%;
 				letter-spacing: 0.02em;
 
 				color: #3c55ab;
+
+				margin-bottom: 4px;
 			}
 
 			.school-list-date {
@@ -633,7 +702,7 @@
 	.information {
 		text-align: center;
 		color: #3c55ab;
-		padding: 0 16px;
+		padding: 0 16px 48px;
 
 		> h2 {
 			font-family: 'IBM Plex Sans Thai Looped';
@@ -684,6 +753,201 @@
 			line-height: 136%;
 			color: #9daad5;
 			margin-top: 8px;
+		}
+	}
+
+	.origin {
+		background: #3c55ab;
+		border-radius: 24px 24px 0px 0px;
+		padding: 24px 16px;
+		color: #fff;
+		text-align: center;
+
+		p {
+			font-size: 0.8125rem;
+			line-height: 136%;
+		}
+
+		a {
+			color: #fff;
+		}
+
+		h3 {
+			font-family: 'Mitr';
+			font-style: normal;
+			font-weight: 500;
+			font-size: 1rem;
+			line-height: 125%;
+			letter-spacing: 0.02em;
+			color: #fff;
+
+			gap: 12px;
+			margin: 24px 0;
+		}
+
+		.data-cards {
+			gap: 16px;
+			flex-direction: column;
+			align-items: stretch;
+			margin-bottom: 24px;
+
+			@media screen and (min-width: 992px) {
+				flex-direction: row;
+				margin-inline: calc(-50% + 56px);
+			}
+
+			> .card {
+				h4 {
+					font-family: 'Mitr';
+					font-style: normal;
+					font-weight: 500;
+					font-size: 1rem;
+					line-height: 125%;
+					letter-spacing: 0.02em;
+					margin-bottom: 8px;
+				}
+
+				background: #fff5dd;
+				box-shadow: 0px 1px 4px rgba(12, 22, 107, 0.2);
+				border-radius: 5px;
+				padding: 12px 16px;
+				color: #3c55ab;
+				font-size: 13px;
+				line-height: 136%;
+				flex: 1 1 0;
+			}
+		}
+	}
+
+	.open-join-learn {
+		padding: 40px 16px;
+		text-align: center;
+		color: #3c55ab;
+
+		flex-direction: column;
+		gap: 24px;
+
+		h2 {
+			font-family: 'Mitr';
+			font-style: normal;
+			font-weight: 500;
+			font-size: 1.25rem;
+			line-height: 125%;
+			letter-spacing: 0.02em;
+		}
+	}
+
+	.feature {
+		padding: 24px 16px 0;
+		margin-bottom: 20px;
+		text-align: center;
+		background: #3c55ab;
+		border-radius: 0px 0px 24px 24px;
+		color: #fff;
+		flex-direction: column;
+		gap: 24px;
+
+		.feature-card-container {
+			gap: 16px;
+			align-items: stretch;
+			flex-direction: column;
+
+			@media screen and (min-width: 992px) {
+				flex-direction: row;
+				margin-inline: calc(-50% + 56px);
+			}
+
+			.feature-card {
+				background: rgba(255, 255, 255, 0.1);
+				border-radius: 5px;
+				padding: 16px;
+				font-size: 0.8125rem;
+				line-height: 136%;
+				flex: 1 1 0;
+
+				h3 {
+					font-family: 'Mitr';
+					font-style: normal;
+					font-weight: 500;
+					font-size: 1rem;
+					line-height: 125%;
+					text-align: center;
+					letter-spacing: 0.02em;
+					gap: 12px;
+					justify-content: center;
+					margin-bottom: 8px;
+				}
+			}
+		}
+	}
+
+	.ending {
+		margin-top: 32px;
+		padding: 24px;
+		color: #3c55ab;
+		flex-direction: column;
+		text-align: center;
+
+		.social-links {
+			gap: 20px;
+			margin: 24px 0;
+		}
+
+		.more-links {
+			flex-direction: column;
+			gap: 4px;
+
+			a {
+				color: #3c55ab;
+			}
+		}
+
+		strong,
+		.bold {
+			font-weight: 500;
+		}
+	}
+
+	.footer {
+		flex-direction: column;
+		padding: 24px 16px 40px;
+
+		background: #0c166b;
+		border-radius: 24px 24px 0px 0px;
+		color: #fff;
+		text-align: center;
+		font-size: 0.8125rem;
+		line-height: 136%;
+
+		gap: 16px;
+
+		.credit {
+			gap: 10px;
+		}
+
+		p {
+			margin-top: 8px;
+		}
+	}
+
+	@media screen and (min-width: 992px) {
+		.header,
+		.school-list-container,
+		.information,
+		.origin,
+		.open-join-learn,
+		.feature,
+		.ending,
+		.footer {
+			padding-inline: 25%;
+		}
+
+		.hide-desktop {
+			display: none;
+		}
+
+		.information > h2 {
+			gap: 32px;
 		}
 	}
 </style>
