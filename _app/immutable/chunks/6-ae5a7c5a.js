44f87370507b1cb535f466f199f4acad8687da27
rootEnginear <suthep.chanchuphol@outlook.com>
2022-12-15 10:21:59 +0000

    Update gh-pages to output generated at 5a02d94

---
+++ _app/immutable/chunks/6-ae5a7c5a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/school/_schoolId_/data/_page.svelte-e3944e24.js";export{t as component};
--- _app/immutable/chunks/6-d9561382.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/school/_schoolId_/data/_page.svelte-9dfc8cab.js";export{t as component};
